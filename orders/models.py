861378c7f738247051262065f1b72690c8375345
Richard Kovachev <thedarkemperorbg@gmail.com>
2018-10-14 20:26:07 +0300

    Add account login/logout/register. Create Shop models add them to admin

--- orders/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Products(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
---
+++ shop/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import ImageField
+from imagekit.models import ImageSpecField
+from pilkit.processors import ResizeToFill
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        ordering = ('name',)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
+    price = models.FloatField(default=0)
+    category = models.ForeignKey('Category', verbose_name='Product Category', blank=False, on_delete=models.CASCADE)
+    image = ImageField(upload_to='product_images')
+    thumbnail = ImageSpecField(source='image',
+                               processors=[ResizeToFill(100, 50)],
+                               format='JPEG',
+                               options={'quality': 60})
+
+
+class Order(models.Model):
+    order_date = models.DateTimeField('date ordered')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    delivery_detiles = models.CharField(max_length=400)
+    cost = models.FloatField(default=0)
+    products = models.ManyToManyField(Product, blank=True)
